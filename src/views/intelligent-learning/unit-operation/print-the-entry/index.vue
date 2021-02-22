b8f6f5042bef778b473c49b933f74bc7d4073877
is-lyx <60839565+is-lyx@users.noreply.github.com>
2021-02-22 16:51:01 +0800

    feat add-student button in the wrong-topic-test print-the-entry page
    新增错题测试打印录入页面的添加学生功能按钮

--- src/views/intelligent-learning/unit-operation/print-the-entry/index.vue
+++ src/views/intelligent-learning/unit-operation/print-the-entry/index.vue
@@ -1,55 +1,41 @@
 <template>
-  <div style="padding:30px;">
+  <div style="padding: 30px">
     <el-card shadow="never">
       <div>
         <i class="el-icon-search" />
         <span>筛选搜索</span>
         <el-button
-          style="float: right;"
+          style="float: right"
           type="primary"
           size="small"
           @click="findPreTestListData"
         >查询</el-button>
         <el-button
-          style="float: right;margin-right: 15px;"
+          style="float: right; margin-right: 15px"
           size="small"
           @click="handleResetSearch"
         >重置</el-button>
       </div>
-      <div style="margin-top: 15px;">
-        <el-form
-          :inline="true"
-          :model="form"
-          size="small"
-          label-width="140px"
-        >
-          <div style="text-align: center;">
+      <div style="margin-top: 15px">
+        <el-form :inline="true" :model="form" size="small" label-width="140px">
+          <div style="text-align: center">
             <el-form-item label="状态：">
               <el-select
                 v-model="form.state"
                 placeholder="请选择状态"
                 clearable
-                style="width: 203px;"
+                style="width: 203px"
                 @change="findPreTestListData"
               >
-                <el-option
-                  label="全部"
-                  value="0"
-                />
-                <el-option
-                  label="已完成"
-                  value="1"
-                />
-                <el-option
-                  label="未完成"
-                  value="2"
-                />
+                <el-option label="全部" value="0" />
+                <el-option label="已完成" value="1" />
+                <el-option label="未完成" value="2" />
               </el-select>
             </el-form-item>
             <el-form-item label="学生姓名：">
               <el-input
                 v-model="form.studentName"
-                style="width: 203px;"
+                style="width: 203px"
                 placeholder="请输入学生姓名"
                 @keyup.enter.native="findPreTestListData"
               />
@@ -59,7 +45,7 @@
       </div>
     </el-card>
 
-    <div style="margin-top: 10px;">
+    <div style="margin-top: 10px">
       <el-button
         type="primary"
         size="medium"
@@ -72,29 +58,64 @@
     <PageTable
       ref="dataTable"
       :data="tableData"
-      :page-sizes="[5,10]"
+      :page-sizes="[5, 10]"
       :page-size="5"
-
       :paging="true"
       :dynamic-column-setting="true"
       :column-visibles="columnVisibles"
-      :default-sort="{prop: 'grade', order: 'descending'}"
-      :hiden-column-indexs="[0,1,2,3,6]"
-
+      :default-sort="{ prop: 'grade', order: 'descending' }"
+      :hiden-column-indexs="[0, 1, 2, 3, 6]"
       :show-always-show-column-in-checkbox="true"
     >
       <el-table-column v-if="columnVisibles[0]" label="ID" prop="id" />
-      <el-table-column v-if="columnVisibles[1]" label="学生ID" prop="studentID" />
+      <el-table-column
+        v-if="columnVisibles[1]"
+        label="学生ID"
+        prop="studentID"
+      />
       <el-table-column v-if="columnVisibles[2]" label="班级ID" prop="classID" />
       <el-table-column v-if="columnVisibles[3]" label="班级" prop="class" />
-      <el-table-column v-if="columnVisibles[4]" label="学生姓名" prop="studentName" />
-      <el-table-column v-if="columnVisibles[5]" label="作业名称" prop="homeworkName" />
-      <el-table-column v-if="columnVisibles[6]" label="创建时间" prop="creationTime" />
-      <el-table-column v-if="columnVisibles[7]" label="录入时间" prop="entryTime" />
-      <el-table-column v-if="columnVisibles[8]" label="题量" prop="amountOfTheQuestions" />
-      <el-table-column v-if="columnVisibles[9]" label="完成情况" prop="finishState" />
-      <el-table-column v-if="columnVisibles[10]" label="分数" sortable prop="grade" />
-      <el-table-column v-if="columnVisibles[11]" label="用时" prop="usingTime" />
+      <el-table-column
+        v-if="columnVisibles[4]"
+        label="学生姓名"
+        prop="studentName"
+      />
+      <el-table-column
+        v-if="columnVisibles[5]"
+        label="作业名称"
+        prop="homeworkName"
+      />
+      <el-table-column
+        v-if="columnVisibles[6]"
+        label="创建时间"
+        prop="creationTime"
+      />
+      <el-table-column
+        v-if="columnVisibles[7]"
+        label="录入时间"
+        prop="entryTime"
+      />
+      <el-table-column
+        v-if="columnVisibles[8]"
+        label="题量"
+        prop="amountOfTheQuestions"
+      />
+      <el-table-column
+        v-if="columnVisibles[9]"
+        label="完成情况"
+        prop="finishState"
+      />
+      <el-table-column
+        v-if="columnVisibles[10]"
+        label="分数"
+        sortable
+        prop="grade"
+      />
+      <el-table-column
+        v-if="columnVisibles[11]"
+        label="用时"
+        prop="usingTime"
+      />
       <el-table-column v-if="columnVisibles[12]" label="操作" min-width="100">
         <template slot-scope="scope">
           <el-button
@@ -197,7 +218,6 @@
         <el-button type="primary" @click="add">确定</el-button>
       </div>
     </el-dialog>
-
   </div>
 </template>
 
@@ -243,10 +263,20 @@ export default {
       // 模拟后台数据
       const data = []
       for (var i = 1; i <= 23; i++) {
-        data.push({ id: i, studentID: i, classID: i, class: i,
-          studentName: i, homeworkName: i, creationTime: i, entryTime: i,
-          amountOfTheQuestions: i, finishState: i,
-          grade: i, usingTime: i })
+        data.push({
+          id: i,
+          studentID: i,
+          classID: i,
+          class: i,
+          studentName: i,
+          homeworkName: i,
+          creationTime: i,
+          entryTime: i,
+          amountOfTheQuestions: i,
+          finishState: i,
+          grade: i,
+          usingTime: i
+        })
       }
 
       this.tableData = data
